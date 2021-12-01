ad6407b1c6158d8758793b70256903fec2b31cb9
Stijn van der Pol <stijn@hackerone.com>
2021-12-01 13:54:32 +0100

    initial commit

---
+++ stijn/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.ts",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    library: 'adventOfCode2021',
+    libraryTarget: 'commonjs2'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: "babel-loader",
+        exclude: /node_modules/
+      }, {
+        test: /\.js$/,
+        loader: "source-map-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ "js", "ts" ]
+  }
+};
