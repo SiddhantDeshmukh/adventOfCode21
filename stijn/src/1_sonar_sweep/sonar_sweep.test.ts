7c321db53c0a8b1e2ecc3e979db0db672671f103
Stijn van der Pol <stijn@hackerone.com>
2021-12-01 16:33:56 +0100

    add second challenge solution for day 1

--- stijn/src/1_sonar_sweep/sonar_sweep.test.ts
+++ stijn/src/1_sonar_sweep/sonar_sweep.test.ts
@@ -1,13 +1,27 @@
-import { sonarSweep } from "./sonar_sweep";
+import { sonarSweepPartOne, sonarSweepPartTwo } from "./sonar_sweep";
 
 describe("Day 1: Sonar Sweep", () => {
-  it("handles arrays with one or zero indices", () => {
-    expect(sonarSweep([])).toEqual(0);
-    expect(sonarSweep([100])).toEqual(0);
+  describe("Part One: count the number of times a depth measurement increases from the previous measurement.", () => {
+    it("handles arrays with one or zero indices", () => {
+      expect(sonarSweepPartOne([])).toEqual(0);
+      expect(sonarSweepPartOne([100])).toEqual(0);
+    });
+
+    it("returns the correct number of increases", () => {
+      const data = [10, 15, 20, 15, 20];
+      expect(sonarSweepPartOne(data)).toEqual(3);
+    });
   });
 
-  it("returns the correct number of increases", () => {
-    const data = [10, 15, 20, 15, 20];
-    expect(sonarSweep(data)).toEqual(3);
+  describe("Part Two: count the number of times the sum of measurements in this sliding window increases from the previous sum", () => {
+    it("handles arrays with less than 4 indices", () => {
+      const data = [10, 20, 5];
+      expect(sonarSweepPartTwo(data)).toEqual(0);
+    });
+
+    it("returns the correct number of increase", () => {
+      const data = [199, 200, 208, 210, 220, 5, 100];
+      expect(sonarSweepPartTwo(data)).toEqual(2);
+    });
   });
 });
--- stijn/src/1_sonar_sweep/sonar_sweep.ts
+++ stijn/src/1_sonar_sweep/sonar_sweep.ts
@@ -3,7 +3,7 @@ import * as dataset from "./data.json";
 /**
  * Returns the number of times a value in the array is larger than the value of the previous index.
  */
-export const sonarSweep = (measurements: number[]): number => {
+export const sonarSweepPartOne = (measurements: number[]): number => {
   if (measurements.length <= 1) return 0;
   let measurementIncreases = 0;
 
@@ -19,11 +19,41 @@ export const sonarSweep = (measurements: number[]): number => {
   return measurementIncreases;
 };
 
+/**
+ * Returns the amount of times the sum of the numbers in a 3-index sliding window are higher than
+ * the previous window.
+ */
+export const sonarSweepPartTwo = (measurements: number[]): number => {
+  if (measurements.length < 4) return 0;
+  let measurementIncreases = 0;
+
+  for (let i = 1; i < measurements.length - 2; i++) {
+    const previousMeasurement =
+      measurements[i - 1] + measurements[i] + measurements[i + 1];
+    const currentMeasurement =
+      measurements[i] + measurements[i + 1] + measurements[i + 2];
+
+    if (previousMeasurement < currentMeasurement) {
+      measurementIncreases += 1;
+    }
+  }
+
+  return measurementIncreases;
+};
+
 export default () => {
   console.log("Day 1. Sonar Sweep");
-  console.log(
-    "Challenge: count the number of times a depth measurement increases from the previous measurement."
-  );
+  console.log("");
 
-  console.log("Answer: ", sonarSweep(dataset.data));
+  console.log(
+    "Challenge 1: count the number of times a depth measurement increases from the previous measurement."
+  );
+  console.log("Answer: ", sonarSweepPartOne(dataset.data));
+  console.log("");
+  console.log(
+    "Challenge 2: count the number of times the sum of measurements in this sliding window increases from the previous sum."
+  );
+  console.log("Answer: ", sonarSweepPartTwo(dataset.data));
+  console.log("");
+  console.log("");
 };
