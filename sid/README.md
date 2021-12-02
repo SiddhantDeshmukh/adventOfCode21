2b2b2a0c2a9740e6d8fb12c985a7efb38606d3bc
Siddhant Deshmukh <sd486@exeter.ac.uk>
2021-12-02 12:36:53 +0100

    day 2 solutions

--- sid/README.md
+++ sid/README.md
@@ -3,7 +3,7 @@
 In this directory are my solutions to the Advent of Code 2021 puzzles done
 in Julia. And I'll write my thoughts about each day's challenges below!
 
-## Day 1
+## Day 1 - Sonar Sweep
 
 Today was focused on finite differencing arrays based on different windows.
 Part 1, quite simply, involved computing a finite difference of the input
@@ -16,3 +16,12 @@ over the input in 3-element windows, summing each one and adding it to
 an array. At the end, I applied the same finite difference
 positive-counting method on this new array whose elements were the window
 sums. All in all, a good start!
+
+## Day 2 - Dive!
+
+I solved this using vectors, assigning coordinates [pos, depth] for part 1
+and [pos, depth, aim] for part 2. The instructions for the submarine are
+stored in a dictionary. In part 1, this dictionary contains the array that
+should be multipled by the value 'X' to get the new [pos, depth]. In part 2,
+the dictionary contains the 'forward', 'down', 'up' functions to directly
+update the coordinates.
---
+++ sid/src/day_2.jl
@@ -0,0 +1,41 @@
+# https://adventofcode.com/2021/day/2
+using AdventOfCode
+
+input = readlines("data/day_2.txt")
+
+function part_1(input)
+  # Given set of instructions, find final depth and horizontal direction
+  # Multply end result together
+  # 'coord' is [horizontal position, depth]
+  coord = [0, 0]
+  # Define directions
+  directions = Dict("forward" => [1, 0], "down" => [0, 1], "up" => [0, -1])
+  for instruction in input
+    direction, value = split(instruction, " ")
+    coord += (directions[direction] .* parse(Int, value))
+  end
+  return prod(coord)
+end
+@info part_1(input)
+
+function part_2(input)
+  # Add 'aim' quantity as third index in 'coord'
+  # 'coord' is [horizontal position, depth, aim]
+  coord = [0, 0, 0]
+  # Define directions
+  forward(coord, x) = [coord[1] + x, coord[2] + coord[3] * x, coord[3]] 
+  down(coord, x) = coord + [0, 0, 1].*x
+  up(coord, x) = coord + [0, 0, -1].*x
+  aims = Dict(
+    "forward" => forward,
+    "down" => down,
+    "up" => up
+  )
+  
+  for instruction in input
+    direction, value = split(instruction, " ")
+    coord = aims[direction](coord, parse(Int, value))
+  end
+  return prod(coord[1:2])
+end
+@info part_2(input)
