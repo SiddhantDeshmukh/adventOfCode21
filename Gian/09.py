0124d6e5b7a430abacd02ca91c76ea6bb6da063a
GiMattia <95360639+GiMattia@users.noreply.github.com>
2021-12-10 08:42:03 +0100

    Update 09.py

--- Gian/09.py
+++ Gian/09.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-file  = open('09_ex.txt', 'r')
+file  = open('09.txt', 'r')
 lines = file.readlines()
 grid = np.ndarray(shape=(len(lines),len(lines[0].rstrip())), dtype=int)
 
@@ -40,7 +40,6 @@ for i in range(len(grid)):
 print(count)
 
 grid = np.ones((len(lines) + 2,len(lines[0].rstrip()) + 2))*9
-gridtest = np.zeros((len(lines) + 2,len(lines[0].rstrip()) + 2))
 
 sizes = []
 for i in range(1,len(lines) + 1):
@@ -48,36 +47,42 @@ for i in range(1,len(lines) + 1):
     for j in range(1,len(call) + 1):
         grid[i,j] = call[j-1]
 
+sizes  = []
 for i in range(len(low)):
-    size  = 1
-    found9 = 1
+    gridtest = np.zeros((len(lines) + 2,len(lines[0].rstrip()) + 2))
+    zoneso = []
+    zonesn = []
     x9 = x[i] + 1
     y9 = y[i] + 1
-    xmin = x[i] + 1
-    xmax = x[i] + 1
-    ymin = y[i] + 1
-    ymax = y[i] + 1
     gridtest[x9,y9] = 1
-    checkl = 1
-    checkr = 1
-    checku = 1
-    checkd = 1
-    while checkl == 1:
-        ymin = ymin - 1
-        if grid[x9,ymin] < 9:
-            size = size + 1
-            gridtest[x9,ymin] = 1
-        else:
-            checkl = 0
-    while checkr == 1:
-        ymax = ymax + 1
-        if grid[x9,ymax] < 9:
-            size = size + 1
-            gridtest[x9,ymax] = 1
-        else:
-            checkr = 0
-    print(size)        
+    zoneso.append([x9,y9])
+    zonesn.append([x9,y9])
+    while len(zoneso) > 0:
+        for j in zoneso:
+            scrh1 = grid[j[0],j[1]+1]
+            scrh2 = gridtest[j[0],j[1]+1]
+            if scrh1 < 9 and scrh2 < 1:
+                zonesn.append([j[0],j[1]+1])
+                gridtest[j[0],j[1]+1] = 1
+            scrh1 = grid[j[0],j[1]-1]
+            scrh2 = gridtest[j[0],j[1]-1]
+            if scrh1 < 9 and scrh2 < 1:
+                zonesn.append([j[0],j[1]-1])
+                gridtest[j[0],j[1]-1] = 1
+            scrh1 = grid[j[0]+1,j[1]]
+            scrh2 = gridtest[j[0]+1,j[1]]
+            if scrh1 < 9 and scrh2 < 1:
+                zonesn.append([j[0]+1,j[1]])
+                gridtest[j[0]+1,j[1]] = 1
+            scrh1 = grid[j[0]-1,j[1]]
+            scrh2 = gridtest[j[0]-1,j[1]]
+            if scrh1 < 9 and scrh2 < 1:
+                zonesn.append([j[0]-1,j[1]])
+                gridtest[j[0]-1,j[1]] = 1
 
+        zonesn.remove(j)
+        zoneso = zonesn
+    sizes.append(np.sum(gridtest))
 
-print(grid)
-print(gridtest)
+size = np.sort(sizes)
+print(int(size[-1]*size[-2]*size[-3]))
